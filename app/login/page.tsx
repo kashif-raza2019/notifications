6dfe162c58df28332b2824fd3cae9d3e448c8614
kashif-raza2019 <kashifraza08012001@gmail.com>
2024-11-06 00:29:05 +0530

    app structure initialized

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@/components/ui/button";
+
+const Login = () => {
+    return (
+        <div >
+            <h3>Hello</h3>
+            <Button>Test Click</Button>
+        </div>
+    );
+};
+export default Login;
