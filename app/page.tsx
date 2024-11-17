51fe122b85d0f93c6663c5b40c25aac9a56b8db2
kashif-raza2019 <kashifraza08012001@gmail.com>
2024-11-17 17:49:36 +0530

    check env variables available to client

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,9 @@
 import { useEffect } from "react";
 import {SERVER_HOST_URL, VAPID_PUBLIC_KEY} from './imports';
 
+console.info("PUBLIC VAPID KEY", VAPID_PUBLIC_KEY);
+console.info("PUBLIC SERVER ONE QUEUE DEV", SERVER_HOST_URL);
+
 export default function Home() {
   useEffect(() => {
     if ("serviceWorker" in navigator) {
